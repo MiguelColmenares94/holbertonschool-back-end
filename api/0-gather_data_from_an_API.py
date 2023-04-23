5ce8f803f1a0411ccf9af18fc9ea613b9979a9f1
Miguel Colmenares <miguelog.alejandro@gmail.com>
2023-04-22 17:45:51 -0700

    style: fix output format according to task requirements

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,33 +10,36 @@ from sys import argv, stderr, exit
 
 def main():
     if len(argv) < 2:
+        print("Usage: {} ID".format(argv[0]))
         exit(1)
 
     employee_id = int(argv[1])
     url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     response = requests.get(url)
+    url_name = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response_name = requests.get(url_name)
+
 
     if response.status_code == 200:
         todos = response.json()
         total_tasks = len(todos)
         completed_tasks = [todo for todo in todos if todo['completed']]
         num_completed_tasks = len(completed_tasks)
-        """employee_name = todos[0]['name']"""
+    else:
+        print("Error fetching TODO list")
 
-    url_name = f"https://jsonplaceholder.typicode.com/users/1"
-    response_name = requests.get(url_name)
 
     if response_name.status_code == 200:
         employee_data = response_name.json()
         if "name" in employee_data:
             employee_name = employee_data.get("name")
-
-        print("Employee {} is done with tasks({}/{}):"
-              .format(employee_name, num_completed_tasks, total_tasks))
-        for task in completed_tasks:
-            print("\t", task['title'])
     else:
-        print("Error fetching TODO list")
+        print("Error fetching employee name")
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_completed_tasks, total_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
 
 
 if __name__ == "__main__":
